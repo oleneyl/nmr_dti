d018735d6ecc230d22cd75c40f79291a176410d9
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-28 14:10:13 +0900

    Use better Command Line Interface

--- prepare_data.py
+++ prepare_data.py
@@ -1,3 +1,19 @@
-from data_utils import create_every_alignment
+from tqdm import tqdm
+from argparse import ArgumentParser
 
-create_every_alignment()
+from data_utils import create_every_alignment, create_joined_data
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('--task', type=str, default='create_all')
+
+    return parser.parse_args()
+
+
+if __name__=='__main__':
+    args = get_args()
+
+    if args.task == 'create_all':
+        create_every_alignment(wrapper=tqdm)
+    elif args.task == 'create_join':
+        create_joined_data(wrapper=tqdm)
