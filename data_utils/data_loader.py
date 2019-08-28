0d3bb0aa67efb85f3520f6cb3d206b3880622483
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-28 16:55:38 +0900

    Add NMR data loader

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -1,6 +1,6 @@
 import json
 from .kernel import AbstractLoader
-
+from .nmr_base import NMRQueryEngine
 
 class LineDataLoader(AbstractLoader):
     def parse_data(self, line):
@@ -14,3 +14,20 @@ class JSONDataLoader(AbstractLoader):
     @classmethod
     def dump_data(self, datum):
         return json.dumps(datum)
+
+
+class NMRDataLoader(JSONDataLoader):
+    def __init__(self, fname, nmr_dir):
+        super(NMRDataLoader, self).__init__(fname)
+        self.nmr_query_engine = NMRQueryEngine(nmr_dir)
+
+    def parse_data(self, line):
+        meta_info = super(NMRDataLoader, self).parse_data(line)
+        hmdb_id = meta_info['hmdb_id']
+        datum = self.nmr_query_engine.query(hmdb_id)
+        if datum != NMRQueryEngine.QUERY_FAIL:
+            freq, rg = datum.get_ft()
+            meta_info['nmr_freq'] = freq
+            meta_info['nmr_rg'] = rg
+
+        return meta_info
