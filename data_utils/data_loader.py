f4812610a1649fdc8468a5c3dc6af140d7147fd4
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-28 14:12:28 +0900

    As PEP8 style

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -1,9 +1,11 @@
 import json
 from .kernel import AbstractLoader
 
+
 class LineDataLoader(AbstractLoader):
     def parse_data(self, line):
         return line.strip()
+
 
 class JSONDataLoader(AbstractLoader):
     def parse_data(self, line):
