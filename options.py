f06b26a65537d126ba4ef300d834df15146f4650
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-29 18:46:58 +0900

    Add option parsing platform

---
+++ options.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+from preprocess.data_utils import add_data_util_args
+from models import add_model_args
+from progress import add_progress_args
+
+
+def get_args():
+    parser = ArgumentParser()
+
+    base_group = parser.add_argument_group('base')
+
+    add_data_util_args(parser)
+    add_model_args(parser)
+    add_progress_args(parser)
+
+    return parser.parse_args()
--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,7 +1,14 @@
 from .hmdb import create_hmdb_alignment
 from .uniprot import create_uniprot_alignment
-from .trainable import join_hmdb_and_uniprot
+from .trainable import join_hmdb_and_uniprot, create_negatives, mix_nmr_into_hmdb
 from .kernel import get_conf, change_configuration
+from .data_reader import JSONDataReader
+import random
+
+def mix_nmr(output_file_name, wrapper = None):
+    conf = get_conf()
+    mix_nmr_into_hmdb(conf['hmdb']['export_endpoint'], conf['nmr']['engine_dir'], output_file_name,
+                      wrapper=wrapper)
 
 def create_every_alignment(wrapper=None):
     print('Export HMBD database...')
@@ -9,7 +16,24 @@ def create_every_alignment(wrapper=None):
     print('Export Uniprot database...')
     create_uniprot_alignment(wrapper=wrapper)
 
-def create_joined_data(wrapper=None):
+
+def shuffle_train_valid_split(data, split_ratio):
+    random.shuffle(data)
+    data_length = len(data)
+    split_point = int(data_length * split_ratio)
+    return data[:split_point], data[split_point:]
+
+
+def create_trainable_data(split_ratio, wrapper=None, use_nmr=False):
     conf = get_conf()
-    join_hmdb_and_uniprot(conf['hmdb']['export_endpoint'], conf['uniprot']['export_endpoint'], conf['trainable']['export_endpoint'],
-                          wrapper=wrapper)
+    hmdb_file = conf['hmdb']['nmr_endpoint'] if use_nmr else conf['hmdb']['export_endpoint']
+    positives = join_hmdb_and_uniprot(hmdb_file, conf['uniprot']['export_endpoint'],
+                                      wrapper=wrapper)
+    negatives = create_negatives(hmdb_file, conf['uniprot']['export_endpoint'],
+                                 size=len(positives), wrapper=wrapper)
+
+    train_data = positives + negatives
+
+    train, valid = shuffle_train_valid_split(train_data, split_ratio)
+    JSONDataReader.save_from_raw(train, conf['trainable']['train'])
+    JSONDataReader.save_from_raw(valid, conf['trainable']['valid'])
