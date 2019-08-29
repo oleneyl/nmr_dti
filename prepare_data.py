dbbc6dc93614f0392adffa2120b93599e6f20312
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-29 18:47:19 +0900

    Add additional tasks in preprocess

--- prepare_data.py
+++ prepare_data.py
@@ -1,12 +1,17 @@
 from tqdm import tqdm
 from argparse import ArgumentParser
 
-from data_utils import create_every_alignment, create_joined_data, change_configuration
+from preprocess import create_every_alignment, create_trainable_data, change_configuration, mix_nmr
+from preprocess.tokenize import create_protein_vocab
 
 def get_args():
     parser = ArgumentParser()
     parser.add_argument('--task', type=str, default='create_all')
     parser.add_argument('--conf', type=str, default='')
+
+    parser.add_argument('--output_prefix', type=str)
+    parser.add_argument('--split_ratio', type=float, default=0.95)
+    parser.add_argument('--nmr', action='store_true')
     return parser.parse_args()
 
 
@@ -19,4 +24,8 @@ if __name__=='__main__':
     if args.task == 'create_all':
         create_every_alignment(wrapper=tqdm)
     elif args.task == 'create_join':
-        create_joined_data(wrapper=tqdm)
+        create_trainable_data(args.split_ratio, wrapper=tqdm, use_nmr=args.nmr)
+    elif args.task == 'protein_vocab':
+        create_protein_vocab(args.output_prefix)
+    elif args.task == 'mix_nmr':
+        mix_nmr(args.output_prefix + '.mix_nmr', wrapper=tqdm)
