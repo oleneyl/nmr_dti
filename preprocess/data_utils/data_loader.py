a551b19ca73f1a8f98c34f48b3d3b15101b38a7e
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-08-30 13:09:57 +0900

    Fix bug and return (train, valid) loaders
    bug fixed:: batch will be reset after yield

--- preprocess/data_utils/data_loader.py
+++ preprocess/data_utils/data_loader.py
@@ -1,29 +1,41 @@
 from ..data_reader import JSONDataReader, NMRDataReader, RestrictiveNMRDataReader
+from ..kernel import get_conf
 from .numpy_adapter import get_adapter
+import numpy as np
 
 
 def data_loader_args(parser):
+    base_conf = get_conf()
     group = parser.add_argument_group('data_loader')
-    group.add_argument('--data_file_name', type=str)
+    group.add_argument('--train_file_name', type=str, default=base_conf['trainable']['train'])
+    group.add_argument('--valid_file_name', type=str, default=base_conf['trainable']['valid'])
     group.add_argument('--nmr_dir', type=str)
-    group.add_argument('--batch_size', type=str)
+    group.add_argument('--batch_size', type=int)
     group.add_argument('--protein_sequence_length', type=int, default=200)
     group.add_argument('--chemical_sequence_length', type=int, default=200)
 
 
 def get_data_loader(args):
-    return GeneralDataLoader(args.data_file_name, args.nmr_dir,
-                             batch_size=args.batch_size,
-                             protein_sequence_length=args.protein_sequence_length,
-                             chemical_sequence_length=args.chemical_sequence_length,
-                             adapter=get_adapter(args))
+    train_data_loader = GeneralDataLoader(args.train_file_name, args.nmr_dir,
+                                          batch_size=args.batch_size,
+                                          protein_sequence_length=args.protein_sequence_length,
+                                          chemical_sequence_length=args.chemical_sequence_length,
+                                          adapter=get_adapter(args))
+
+    valid_data_loader = GeneralDataLoader(args.valid_file_name, args.nmr_dir,
+                                          batch_size=args.batch_size,
+                                          protein_sequence_length=args.protein_sequence_length,
+                                          chemical_sequence_length=args.chemical_sequence_length,
+                                          adapter=get_adapter(args))
+
+    return train_data_loader, valid_data_loader
 
 
 class GeneralDataLoader(object):
     def __init__(self, data_file_name, nmr_dir, batch_size=1,
                  protein_sequence_length=1000,
                  chemical_sequence_length=1000,
-                 adapter=lambda x: x
+                 adapter=lambda x: x,
                  ):
         self.batch_size = batch_size
         self.data_reader = JSONDataReader(data_file_name)
@@ -34,14 +46,14 @@ class GeneralDataLoader(object):
     def __iter__(self):
         batch = []
         for bind, protein_indices, chemical_indices, nmr_values in self.adapter(self.data_reader):
-            if len(batch) < self.batch_size:
-                if len(protein_indices) < self.protein_sequence_length:
-                    protein_indices = protein_indices + [0 for x in range(self.protein_sequence_length -
-                                                                          len(protein_indices))]
-                if len(chemical_indices) < self.protein_sequence_length:
-                    chemical_indices = chemical_indices + [0 for x in range(self.chemical_sequence_length -
-                                                                          len(chemical_indices))]
-                batch.append([bind, protein_indices[:self.protein_sequence_length],
-                              chemical_indices[:self.chemical_sequence_length], nmr_values])
-            else:
-                yield list(zip(*batch))
+            if len(protein_indices) < self.protein_sequence_length:
+                protein_indices = protein_indices + [0 for x in range(self.protein_sequence_length -
+                                                                      len(protein_indices))]
+            if len(chemical_indices) < self.protein_sequence_length:
+                chemical_indices = chemical_indices + [0 for x in range(self.chemical_sequence_length -
+                                                                        len(chemical_indices))]
+            batch.append([[bind], protein_indices[:self.protein_sequence_length],
+                          chemical_indices[:self.chemical_sequence_length], nmr_values.tolist()])
+            if len(batch) == self.batch_size:
+                yield [np.array(x) for x in zip(*batch)]
+                batch = []
