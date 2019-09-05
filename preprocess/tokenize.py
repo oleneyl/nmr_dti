ab737112afcf9b4d985447d001d10d7624977d43
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-09-05 18:12:14 +0900

    Add chemical vocab parser

--- preprocess/tokenize.py
+++ preprocess/tokenize.py
@@ -4,8 +4,8 @@ from .data_reader import JSONDataReader
 
 TEMP_FILE_NAME = '.temp-tknyze-nmr_infer'
 
+
 def create_protein_vocab(output_prefix):
-    sequences = []
     #Create protein vocabulary
     conf = get_conf()
     proteins = JSONDataReader(conf['uniprot']['export_endpoint'])
@@ -14,3 +14,10 @@ def create_protein_vocab(output_prefix):
             f.write(protein['sequence'].strip()+'\n')
 
     spm.SentencePieceTrainer.Train(f'--input={TEMP_FILE_NAME} --model_prefix={output_prefix} --vocab_size=1000')
+
+def create_chemical_vocab(output_prefix):
+    sequences = []
+    #Create chemical vocabulary
+    conf = get_conf()
+    INPUT='/DATA/meson324/InterpretableDTIP/data/train/chem.repr'
+    spm.SentencePieceTrainer.Train(f'--input={INPUT} --model_prefix={output_prefix} --vocab_size=1000 --hard_vocab_limit=false')
